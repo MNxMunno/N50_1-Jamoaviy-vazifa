9619c15cc036ef8419fc11fd14425a5443992320
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-01-29 19:20:19 +0500

    MacBookPro-21

--- js/main.js
+++ js/main.js
@@ -10,3 +10,22 @@ const menuBtn = document.querySelector(".menu");
 menuBtn.addEventListener("click", () => {
   header.classList.toggle("open");
 });
+
+
+
+window.addEventListener('scroll', function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById('backtop');
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = '20px';
+  } else {
+    backtop.style.bottom = '-50px';
+  }
+}
